81c424a144d89d506277178c2ff0af664e62c1c0
yanJuicy <hanjub3@gmail.com>
2023-06-25 15:03:33 +0900

    조인 - 기본 조인

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -16,7 +16,7 @@ import javax.persistence.PersistenceContext;
 
 import java.util.List;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static study.querydsl.entity.QMember.member;
 import static study.querydsl.entity.QTeam.team;
 
@@ -235,6 +235,35 @@ public class QuerydslBasicTest {
 
 		assertThat(teamB.get(team.name)).isEqualTo("teamB");
 		assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+	}
+
+	@Test
+	public void join() {
+		List<Member> result = queryFactory
+				.selectFrom(member)
+				.join(member.team, team)
+				.where(team.name.eq("teamA"))
+				.fetch();
+
+		assertThat(result)
+				.extracting("username")
+				.containsExactly("member1", "member2");
+	}
+
+	@Test
+	public void theta_join() {
+		em.persist(new Member("teamA"));
+		em.persist(new Member("teamB"));
+
+		List<Member> result = queryFactory
+				.select(member)
+				.from(member, team)
+				.where(member.username.eq(team.name))
+				.fetch();
+
+		assertThat(result)
+				.extracting("username")
+				.containsExactly("teamA", "teamB");
 	}
 
 }
