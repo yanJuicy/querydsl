001806569a25647e1fa3af117196652acb794402
yanJuicy <hanjub3@gmail.com>
2023-06-28 13:32:13 +0900

    조인 - 페치 조인

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -12,7 +12,9 @@ import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
 
 import java.util.List;
 
@@ -24,106 +26,106 @@ import static study.querydsl.entity.QTeam.team;
 @Transactional
 public class QuerydslBasicTest {
 
-	@PersistenceContext
-	EntityManager em;
+    @PersistenceContext
+    EntityManager em;
 
-	JPAQueryFactory queryFactory;
+    JPAQueryFactory queryFactory;
 
-	@BeforeEach
-	public void before() {
-		queryFactory = new JPAQueryFactory(em);
+    @BeforeEach
+    public void before() {
+        queryFactory = new JPAQueryFactory(em);
 
-		Team teamA = new Team("teamA");
-		Team teamB = new Team("teamB");
-		em.persist(teamA);
-		em.persist(teamB);
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
 
-		Member member1 = new Member("member1", 10, teamA);
-		Member member2 = new Member("member2", 20, teamA);
-		Member member3 = new Member("member3", 30, teamB);
-		Member member4 = new Member("member4", 40, teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
 
-		em.persist(member1);
-		em.persist(member2);
-		em.persist(member3);
-		em.persist(member4);
-	}
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
 
-	@Test
-	public void startJPQL() {
-		String qlString =
-				"SELECT m FROM Member m " +
-						"WHERE m.username = :username";
+    @Test
+    public void startJPQL() {
+        String qlString =
+                "SELECT m FROM Member m " +
+                        "WHERE m.username = :username";
 
-		Member findMember = em.createQuery(qlString, Member.class)
-				.setParameter("username", "member1")
-				.getSingleResult();
+        Member findMember = em.createQuery(qlString, Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
 
-		assertThat(findMember.getUsername()).isEqualTo("member1");
-	}
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
 
-	@Test
-	public void startQuerydsl() {
-		JPAQueryFactory queryFactory = new JPAQueryFactory(em);
-		QMember m = new QMember("m");
+    @Test
+    public void startQuerydsl() {
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QMember m = new QMember("m");
 
-		Member findMember = queryFactory
-				.select(m)
-				.from(m)
-				.where(m.username.eq("member1"))
-				.fetchOne();
+        Member findMember = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1"))
+                .fetchOne();
 
-		assertThat(findMember.getUsername()).isEqualTo("member1");
-	}
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
 
-	@Test
-	public void startQuerydsl2() {
-		QMember m = new QMember("m");
+    @Test
+    public void startQuerydsl2() {
+        QMember m = new QMember("m");
 
-		Member findMember = queryFactory
-				.select(m)
-				.from(m)
-				.where(m.username.eq("member1"))
-				.fetchOne();
+        Member findMember = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1"))
+                .fetchOne();
 
-		assertThat(findMember.getUsername()).isEqualTo("member1");
-	}
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
 
-	@Test
-	public void startQuerydsl3() {
-		Member findMember = queryFactory
-				.select(member)
-				.from(member)
-				.where(member.username.eq("member1"))
-				.fetchOne();
+    @Test
+    public void startQuerydsl3() {
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
 
-		assertThat(findMember.getUsername()).isEqualTo("member1");
-	}
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
 
-	@Test
-	public void search() {
-		Member findMember = queryFactory
-				.selectFrom(member)
-				.where(member.username.eq("member1")
-						.and(member.age.eq(10)))
-				.fetchOne();
+    @Test
+    public void search() {
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1")
+                        .and(member.age.eq(10)))
+                .fetchOne();
 
-		assertThat(findMember.getUsername()).isEqualTo("member1");
-	}
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
 
-	@Test
-	public void searchAndParam() {
-		List<Member> result1 = queryFactory
-				.selectFrom(member)
-				.where(member.username.eq("member1"),
-						member.age.eq(10))
-				.fetch();
+    @Test
+    public void searchAndParam() {
+        List<Member> result1 = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"),
+                        member.age.eq(10))
+                .fetch();
 
-		assertThat(result1.size()).isEqualTo(1);
-	}
+        assertThat(result1.size()).isEqualTo(1);
+    }
 
-	@Test
-	public void resultFetch() {
+    @Test
+    public void resultFetch() {
 //        // List
 //        List<Member> fetch = queryFactory
 //                .selectFrom(member)
@@ -139,161 +141,198 @@ public class QuerydslBasicTest {
 //                .selectFrom(member)
 //                .fetchFirst();
 
-		// 페이징에서 사용
-		QueryResults<Member> results = queryFactory
-				.selectFrom(member)
-				.fetchResults();
+        // 페이징에서 사용
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .fetchResults();
 
-		// count 쿼리로 변경
-		long count = queryFactory
-				.selectFrom(member)
-				.fetchCount();
-	}
+        // count 쿼리로 변경
+        long count = queryFactory
+                .selectFrom(member)
+                .fetchCount();
+    }
 
-	@Test
-	public void sort() {
-		em.persist(new Member(null, 100));
-		em.persist(new Member("member5", 100));
-		em.persist(new Member("member6", 100));
+    @Test
+    public void sort() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
 
-		List<Member> result = queryFactory
-				.selectFrom(member)
-				.where(member.age.eq(100))
-				.orderBy(member.age.desc(), member.username.asc().nullsLast())
-				.fetch();
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
 
-		Member member5 = result.get(0);
-		Member member6 = result.get(1);
-		Member memberNull = result.get(2);
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
 
-		assertThat(member5.getUsername()).isEqualTo("member5");
-		assertThat(member6.getUsername()).isEqualTo("member6");
-		assertThat(memberNull.getUsername()).isNull();
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
 
-	}
+    }
 
-	@Test
-	public void paging1() {
-		List<Member> result = queryFactory
-				.selectFrom(member)
-				.orderBy(member.username.desc())
-				.offset(1)
-				.limit(2)
-				.fetch();
+    @Test
+    public void paging1() {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetch();
 
-		assertThat(result.size()).isEqualTo(2);
-	}
+        assertThat(result.size()).isEqualTo(2);
+    }
 
-	@Test
-	public void paging2() {
-		QueryResults<Member> queryResults = queryFactory
-				.selectFrom(member)
-				.orderBy(member.username.desc())
-				.offset(1)
-				.limit(2)
-				.fetchResults();
+    @Test
+    public void paging2() {
+        QueryResults<Member> queryResults = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetchResults();
 
-		assertThat(queryResults.getTotal()).isEqualTo(4);
-		assertThat(queryResults.getLimit()).isEqualTo(2);
-		assertThat(queryResults.getOffset()).isEqualTo(1);
-		assertThat(queryResults.getResults().size()).isEqualTo(2);
-	}
+        assertThat(queryResults.getTotal()).isEqualTo(4);
+        assertThat(queryResults.getLimit()).isEqualTo(2);
+        assertThat(queryResults.getOffset()).isEqualTo(1);
+        assertThat(queryResults.getResults().size()).isEqualTo(2);
+    }
 
-	@Test
-	public void aggregation() {
-		List<Tuple> result = queryFactory
-				.select(member.count(),
-						member.age.sum(),
-						member.age.avg(),
-						member.age.max(),
-						member.age.min())
-				.from(member)
-				.fetch();
+    @Test
+    public void aggregation() {
+        List<Tuple> result = queryFactory
+                .select(member.count(),
+                        member.age.sum(),
+                        member.age.avg(),
+                        member.age.max(),
+                        member.age.min())
+                .from(member)
+                .fetch();
 
-		Tuple tuple = result.get(0);
-		assertThat(tuple.get(member.count())).isEqualTo(4);
-		assertThat(tuple.get(member.age.sum())).isEqualTo(100);
-		assertThat(tuple.get(member.age.avg())).isEqualTo(25);
-		assertThat(tuple.get(member.age.max())).isEqualTo(40);
-		assertThat(tuple.get(member.age.min())).isEqualTo(10);
-	}
+        Tuple tuple = result.get(0);
+        assertThat(tuple.get(member.count())).isEqualTo(4);
+        assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+        assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+        assertThat(tuple.get(member.age.max())).isEqualTo(40);
+        assertThat(tuple.get(member.age.min())).isEqualTo(10);
+    }
 
-	@Test
-	public void group() {
-		List<Tuple> result = queryFactory
-				.select(team.name, member.age.avg())
-				.from(member)
-				.join(member.team, team)
-				.groupBy(team.name)
-				.fetch();
+    @Test
+    public void group() {
+        List<Tuple> result = queryFactory
+                .select(team.name, member.age.avg())
+                .from(member)
+                .join(member.team, team)
+                .groupBy(team.name)
+                .fetch();
 
-		Tuple teamA = result.get(0);
-		Tuple teamB = result.get(1);
+        Tuple teamA = result.get(0);
+        Tuple teamB = result.get(1);
 
-		assertThat(teamA.get(team.name)).isEqualTo("teamA");
-		assertThat(teamA.get(member.age.avg())).isEqualTo(15);
+        assertThat(teamA.get(team.name)).isEqualTo("teamA");
+        assertThat(teamA.get(member.age.avg())).isEqualTo(15);
 
-		assertThat(teamB.get(team.name)).isEqualTo("teamB");
-		assertThat(teamB.get(member.age.avg())).isEqualTo(35);
-	}
+        assertThat(teamB.get(team.name)).isEqualTo("teamB");
+        assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+    }
 
-	@Test
-	public void join() {
-		List<Member> result = queryFactory
-				.selectFrom(member)
-				.join(member.team, team)
-				.where(team.name.eq("teamA"))
-				.fetch();
+    @Test
+    public void join() {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
 
-		assertThat(result)
-				.extracting("username")
-				.containsExactly("member1", "member2");
-	}
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1", "member2");
+    }
 
-	@Test
-	public void theta_join() {
-		em.persist(new Member("teamA"));
-		em.persist(new Member("teamB"));
+    @Test
+    public void theta_join() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
 
-		List<Member> result = queryFactory
-				.select(member)
-				.from(member, team)
-				.where(member.username.eq(team.name))
-				.fetch();
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
 
-		assertThat(result)
-				.extracting("username")
-				.containsExactly("teamA", "teamB");
-	}
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
+    }
 
-	@Test
-	public void join_on_filtering() {
-		List<Tuple> result = queryFactory
-				.select(member, team)
-				.from(member)
-				.leftJoin(member.team, team)
-				.on(team.name.eq("teamA"))
-				.fetch();
+    @Test
+    public void join_on_filtering() {
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(member.team, team)
+                .on(team.name.eq("teamA"))
+                .fetch();
 
-		for (Tuple tuple : result) {
-			System.out.println("tuple = " + tuple);
-		}
-	}
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
 
-	@Test
-	public void join_on_on_relation() {
-		em.persist(new Member("teamA"));
-		em.persist(new Member("teamB"));
+    @Test
+    public void join_on_on_relation() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
 
-		List<Tuple> result = queryFactory
-				.select(member, team)
-				.from(member)
-				.leftJoin(team)
-				.on(member.username.eq(team.name))
-				.fetch();
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team)
+                .on(member.username.eq(team.name))
+                .fetch();
 
-		for (Tuple tuple : result) {
-			System.out.println("tuple = " + tuple);
-		}
-	}
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+
+    @PersistenceUnit
+    EntityManagerFactory emf;
+
+    @Test
+    public void fetchJoinNo() {
+        em.flush();
+        em.clear();
+
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        boolean loaded = emf.getPersistenceUnitUtil()
+                .isLoaded(findMember.getTeam());
+        assertThat(loaded).as("패치 조인 미적용").isFalse();
+    }
+
+    @Test
+    public void fetchJoinUse() {
+        em.flush();
+        em.clear();
+
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+                .fetchJoin()
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        boolean loaded = emf.getPersistenceUnitUtil()
+                .isLoaded(findMember.getTeam());
+        assertThat(loaded).as("페치 조인 적용").isTrue();
+    }
+
 }
