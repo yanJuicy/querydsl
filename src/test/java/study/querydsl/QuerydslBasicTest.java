8f05e59710c984a3d0c4b8d324ac244d3df8eea4
yanJuicy <hanjub3@gmail.com>
2023-07-27 05:58:13 +0900

    case 문

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -2,7 +2,7 @@ package study.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
-import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,10 +16,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceUnit;
-
 import java.util.List;
 
-import static com.querydsl.jpa.JPAExpressions.*;
+import static com.querydsl.jpa.JPAExpressions.select;
 import static org.assertj.core.api.Assertions.assertThat;
 import static study.querydsl.entity.QMember.member;
 import static study.querydsl.entity.QTeam.team;
@@ -383,4 +382,33 @@ public class QuerydslBasicTest {
 		assertThat(result).extracting("age").containsExactly(20, 30, 40);
 	}
 
+	@Test
+	public void basicCase() {
+		List<String> result = queryFactory
+				.select(member.age
+						.when(10).then("열살")
+						.when(20).then("스무살")
+						.otherwise("기타"))
+				.from(member)
+				.fetch();
+
+		for (String s : result) {
+			System.out.println("s = " + s);
+		}
+	}
+
+	@Test
+	public void complexCase() {
+		List<String> result = queryFactory
+				.select(new CaseBuilder()
+						.when(member.age.between(0, 20)).then("0~20살")
+						.when(member.age.between(21, 30)).then("21~30살")
+						.otherwise("기타"))
+				.from(member)
+				.fetch();
+
+		for (String s : result) {
+			System.out.println("s = " + s);
+		}
+	}
 }
